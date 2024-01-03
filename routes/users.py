30ff0a52b4149e996595f7e499298b2069976156
Sanghun Oh <otter.oh@gmail.com>
2024-01-03 17:54:28 +0900

    add somethings

--- routes/users.py
+++ routes/users.py
@@ -8,8 +8,15 @@ from fastapi import Request
 # html 틀이 있는 폴더 위치
 templates = Jinja2Templates(directory="templates/")
 
-# 회원 가입 /users/insert
-@router.get("/insert")  
+# 회원 가입 form /users/form -> users/inserts.html
+@router.get("/form")  
 async def insert(request:Request):
     return templates.TemplateResponse(name="users/inserts.html"
+                                      , context={'request':request})
+
+# 회원 가입 /users/insert -> users/login.html
+@router.get("/insert")  
+async def insert(request:Request):
+    pass    # biz
+    return templates.TemplateResponse(name="users/logins.html"
                                       , context={'request':request})
